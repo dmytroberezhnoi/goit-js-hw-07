1e40a07222f9b11b2ca7d2e427f8b7483eeb0069
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-03-27 23:03:04 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// 1. Створення і рендер розмітки на підставі масиву даних
+
+const listEl = document.querySelector(".gallery");
+const elementGalleryMarkup = createElementGalleryMarkup(galleryItems);
+
+listEl.insertAdjacentHTML("beforeend", elementGalleryMarkup);
+
+function createElementGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+// 2. Реалізація делегування на ul.gallery
+
+listEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  const isGalleryImageEl = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+  const ImgUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${ImgUrl}">
+`);
+
+  instance.show();
+}
+
+// 3. Підключення скрипту і стилів бібліотеки модального вікна
